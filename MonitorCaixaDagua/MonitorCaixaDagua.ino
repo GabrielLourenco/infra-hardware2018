404cc1fbeafa7141a140c0cf90ef161a88d326d5
nanotrigueiro <nanotrigueiro@hotmail.com>
2018-08-13 22:29:33 -0300

    Monotor ajustado

--- MonitorCaixaDagua/MonitorCaixaDagua.ino
+++ MonitorCaixaDagua/MonitorCaixaDagua.ino
@@ -1,29 +1,35 @@
 #define BLYNK_PRINT Serial
 
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <BlynkSimpleEthernet.h>
 
-// Auth Token de conexão com o Blynk
-//char auth[] = "23988228d5244a64910de5ce99fa9894";
-char auth[] = "483f60feb270488d9df7754c35178b1e";
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "78177ea4b1c044be83be484f66d977e6";
 
 #define W5100_CS  10
 
-const int pinoSensor1 = 8; //constante que define o pino digital 8 como do Sensor1.
-const int pinoSensor2 = 9; //constante que define o pino digital 9 como do Sensor2.
-const int pinoSensor3 = 10; //constante que define o pino digital 10 como do Sensor1.
-const int pinoSensor4 = 11; //constante que define o pino digital 11 como do Sensor2.
-const int pinoSensor5 = 12; //constante que define o pino digital 12 como do Sensor1.
-int lastTime = 0;
-int estadoSensor1 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
-int estadoSensor2 = 0; //Variável que conterá os estados do Sensor2 (0 LOW, 1 HIGH).
-int estadoSensor3 = 0; //Variável que conterá os estados do Sensor3 (0 LOW, 1 HIGH).
-int estadoSensor4 = 0; //Variável que conterá os estados do Sensor4 (0 LOW, 1 HIGH).
-int estadoSensor5 = 0; //Variável que conterá os estados do Sensor5 (0 LOW, 1 HIGH).
+const int pinoSensor1 = 2; //constante que define o pino digital 8 como do Sensor1.
+const int pinoSensor2 = 3; //constante que define o pino digital 8 como do Sensor1.
+const int pinoSensor3 = 4; //constante que define o pino digital 8 como do Sensor1.
+const int pinoSensor4 = 5; //constante que define o pino digital 8 como do Sensor1.
+const int pinoSensor5 = 6; //constante que define o pino digital 8 como do Sensor1.
 
-WidgetLED led1(V1); //registra o led1 no virtual pino 1, simbolizando o led de conexão 
-WidgetLED led2(V2); //registra o led2 no virtual pino 2, simbolizando o led da bomba
+int estadoSensor1 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+int estadoSensor2 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+int estadoSensor3 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+int estadoSensor4 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+int estadoSensor5 = 0; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+
+int estadoLed1 = 1; //Variável que conterá os estados do Sensor1 (0 LOW, 1 HIGH).
+
+long lastTime1 = 0;
+long lastTime2 = 0;
+
+WidgetLED led1(V1); //register to virtual pin
+WidgetLED led2(V2); //register to virtual pin
 
 void setup()
 {
@@ -34,47 +40,44 @@ void setup()
 
   pinMode(pinoSensor1,INPUT); //Definindo pino do Sensor1 como de entrada. 
   pinMode(pinoSensor2,INPUT); //Definindo pino do Sensor2 como de entrada.
-  pinMode(pinoSensor3,INPUT); //Definindo pino do Sensor1 como de entrada. 
+  pinMode(pinoSensor3,INPUT); //Definindo pino do Sensor2 como de entrada.
   pinMode(pinoSensor4,INPUT); //Definindo pino do Sensor2 como de entrada.
-  pinMode(pinoSensor5,INPUT); //Definindo pino do Sensor1 como de entrada. 
+  pinMode(pinoSensor5,INPUT); //Definindo pino do Sensor2 como de entrada.
 
   led1.off(); //desligando o led1
-  led2.off(); //desligando o led2
-  
-
-  Blynk.virtualWrite(V0, 0);
-}
-
-void conectarBlynk(){
-  led1.off();
-  bool result = Blynk.connect(1000);
-  if(result){
-    Blynk.run();
-    led1.on();
-  }
-
+  //led2.off(); //desligando o led2
+  Blynk.run();
 }
 
 void loop()
 {
+  //delay(1000);
+
+  if (millis() - lastTime2 > 500) {
+    if (estadoLed1 == HIGH) {
+      estadoLed1 = 0;
+      led1.off();
+    } else {
+      estadoLed1 = 1;
+      led1.on();
+    }
+
+    lastTime2 = millis();
+  }
   
-  //if (millis() - lastTime > 500) {
-    conectarBlynk();
-    
+  if (millis() - lastTime1 > 1000) {
+    //Blynk.run();
     estadoSensor1 = digitalRead(pinoSensor1); //Lendo o estado do pino do Sensor1
     estadoSensor2 = digitalRead(pinoSensor2); //Lendo o estado do pino do Sensor2
-    estadoSensor3 = digitalRead(pinoSensor3); //Lendo o estado do pino do Sensor3
-    estadoSensor4 = digitalRead(pinoSensor4); //Lendo o estado do pino do Sensor4
-    estadoSensor5 = digitalRead(pinoSensor5); //Lendo o estado do pino do Sensor5
-  
-  
-    if (estadoSensor4 == LOW) {
-      led2.on(); //caixa está em 60%, led da bomba acende
-    } else {
-      led2.off();      
-    }
-    
+    estadoSensor3 = digitalRead(pinoSensor3); //Lendo o estado do pino do Sensor2
+    estadoSensor4 = digitalRead(pinoSensor4); //Lendo o estado do pino do Sensor2
+    estadoSensor5 = digitalRead(pinoSensor5); //Lendo o estado do pino do Sensor2
+
     Blynk.virtualWrite(V0, 20*estadoSensor1+20*estadoSensor2+20*estadoSensor3+20*estadoSensor4+20*estadoSensor5);
-    lastTime = millis();
-  //}
+
+    Blynk.run();
+    
+    lastTime1 = millis();
+  }
 }
+
